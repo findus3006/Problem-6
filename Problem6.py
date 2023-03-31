293f6fbc0475d5eae3d06b403a0a738922c87d3f
findus3006 <rolfs.stefan@gmail.com>
2023-03-31 19:11:26 +0200

    minor change

--- Problem6.py
+++ Problem6.py
@@ -19,69 +19,51 @@ dt = m*h;
 t = np.arange(0,10,dt) # time grid
 
 # Naive finite differences
-pnow = np.zeros([len(t),len(x)])
-pnow[0,:]=np.cos(np.pi*x); # Initialize
-pold=np.cos(np.pi*x);
-for i in range(len(t)-1):
-    pnew = NFD(pnow[i,:],pold,h,dt)
-    pold = pnow[i,:];
-    pnow[i+1,1:-1] = pnew;
-
-ana = np.zeros([len(t),len(x)])
-for i in range(len(t)):
-    ana[i] = np.cos(np.pi*x)*np.cos(np.pi*t[i])
-
-fig = plt.figure();
-ax = fig.add_subplot()
-
-def update(i):
-    ax.clear()
-    ax.plot(x,pnow[i,:])
-    ax.plot(x,ana[i,:])
-    ax.set_xlim(-0.5, 0.5)
-    ax.set_ylim(-1, 1)
-    ax.set_xlabel('x')
-    ax.set_ylabel('$\cos(\pi$x)')
-    ax.text(0.35,0.9,f't = {round(t[i],2)}',fontsize='12')
-    ax.legend(['FDTD','Analytical'],loc='upper left')
-    ax.grid()
-
-ani = animation.FuncAnimation(fig, update,frames = np.arange(0, len(t), 100), interval = 33.3)
-plt.show()
-
-# Classic FDTD approach
-# v = np.zeros([len(t),len(x)])
-# v[0,:]=0
-# w = np.zeros([len(t),len(x)-1])
-# w[0,:]=-np.pi*np.sin(np.pi*(x[1:]+h/2))
+# pnow = np.zeros([len(t),len(x)])
+# pnow[0,:]=np.cos(np.pi*x); # Initialize
+# pold=np.cos(np.pi*x);
 # for i in range(len(t)-1):
-#     v[i+1],w[i+1] = FDTD(v[i],w[i],h,dt)
+#     pnew = NFD(pnow[i,:],pold,h,dt)
+#     pold = pnow[i,:];
+#     pnow[i+1,1:-1] = pnew;
 
 # ana = np.zeros([len(t),len(x)])
 # for i in range(len(t)):
-#     ana[i] = -np.pi*np.cos(np.pi*x)*np.sin(np.pi*t[i])
+#     ana[i] = np.cos(np.pi*x)*np.cos(np.pi*t[i])
 
 # fig = plt.figure();
 # ax = fig.add_subplot()
 
 # def update(i):
 #     ax.clear()
-#     ax.plot(x,v[i,:])
+#     ax.plot(x,pnow[i,:])
 #     ax.plot(x,ana[i,:])
 #     ax.set_xlim(-0.5, 0.5)
-#     ax.set_ylim(-4, 4)
+#     ax.set_ylim(-1, 1)
 #     ax.set_xlabel('x')
 #     ax.set_ylabel('$\cos(\pi$x)')
 #     ax.text(0.35,0.9,f't = {round(t[i],2)}',fontsize='12')
 #     ax.legend(['FDTD','Analytical'],loc='upper left')
 #     ax.grid()
 
-# ani = animation.FuncAnimation(fig, update,frames = np.arange(0, len(t), round(1/m)*5), interval = 33.3)
+# ani = animation.FuncAnimation(fig, update,frames = np.arange(0, len(t), 100), interval = 33.3)
 # plt.show()
 
+# Classic FDTD approach
+v = np.zeros([len(t),len(x)])
+v[0,:]=0
+w = np.zeros([len(t),len(x)-1])
+w[0,:]=-np.pi*np.sin(np.pi*(x[1:]+h/2))
+for i in range(len(t)-1):
+    v[i+1],w[i+1] = FDTD(v[i],w[i],h,dt)
+
+ana = np.zeros([len(t),len(x)])
+for i in range(len(t)):
+    ana[i] = -np.pi*np.cos(np.pi*x)*np.sin(np.pi*t[i])
+
 # Error and Norm
-TimeError = np.linalg.norm(pnow - ana,axis=1)
-Norm = np.linalg.norm(pnow,axis=1)
+TimeError = np.linalg.norm(v - ana,axis=1)
+Norm = np.linalg.norm(v,axis=1)
 fig2 = plt.figure();
 ax = fig2.add_subplot()
 ax.semilogy(t,TimeError)
@@ -98,3 +80,20 @@ ax.set_ylabel('Norm')
 ax.set_title('Euclidian norm with time')
 ax.grid()
 
+fig = plt.figure();
+ax = fig.add_subplot()
+
+def update(i):
+    ax.clear()
+    ax.plot(x,v[i,:])
+    ax.plot(x,ana[i,:])
+    ax.set_xlim(-0.5, 0.5)
+    ax.set_ylim(-4, 4)
+    ax.set_xlabel('x')
+    ax.set_ylabel('$\cos(\pi$x)')
+    ax.text(0.35,0.9,f't = {round(t[i],2)}',fontsize='12')
+    ax.legend(['FDTD','Analytical'],loc='upper left')
+    ax.grid()
+
+ani = animation.FuncAnimation(fig, update,frames = np.arange(0, len(t), round(1/m)*5), interval = 33.3)
+plt.show()
